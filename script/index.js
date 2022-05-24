d4956c68bd20a4812fbc609287d5e5ddac1057e9
Mikhail Chernyshev <newmishach@gmail.com>
2022-05-24 22:36:24 +0300

    first

---
+++ script/index.js
@@ -0,0 +1,36 @@
+const editButton = document.querySelector('.profile__button-ed-self')
+const popup = document.querySelector('.popup')
+const closePopupButton = document.querySelector('.popup__close')
+
+const profileTitle = document.querySelector('.profile__name')
+const profileRank = document.querySelector('.profile__second-line')
+const popupName = document.querySelector('.popup__input_name')
+const popupRank = document.querySelector('.popup__input_rank')
+const formElement = document.querySelector('.popup__data')
+
+function openPopup(popup) {
+    popup.classList.add('popup__opened')
+}
+
+function closePopup(popup) {
+    popup.classList.remove('popup__opened')
+}
+
+editButton.addEventListener('click', function() {
+    openPopup(popup)
+    popupName.value = profileTitle.textContent;
+    popupRank.value = profileRank.textContent;
+})
+
+closePopupButton.addEventListener('click', function() {
+    closePopup(popup)
+        // profileTitle.textContent = popupName.value;
+        // profileRank.textContent = popupRank.value;
+})
+
+formElement.addEventListener('submit', function() {
+    event.preventDefault()
+    profileTitle.textContent = popupName.value;
+    profileRank.textContent = popupRank.value;
+    closePopup(popup)
+})
